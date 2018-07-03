6879a9e6a108555a3753a73837f055fa7525e1a7
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-07-03 12:07:26 +0800

    Initial commit

---
+++ Assets/Scripts/ChessBoard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ChessType
+{
+    Black,
+    White,
+}
+
+
+public class ChessBoard : MonoBehaviour
+{
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private static readonly Vector2 startPos = new Vector2(6.58f, 6.45f), chessScale = new Vector2(0.95f, 0.95f)
+        , halfChessScale, offestPos;
+
+    public ChessType chessType = ChessType.Black;
+
+    static Player()
+    {
+        halfChessScale = chessScale / 2;
+        offestPos = new Vector2((startPos.x + halfChessScale.x) / chessScale.x, (startPos.y + halfChessScale.y) / chessScale.y);
+    }
+
+
+    public void Update()
+    {
+        PlayerChess();
+    }
+
+    public void PlayerChess()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            InputPointToAxis(p);
+
+        }
+    }
+
+    private Vector2Int InputPointToAxis(Vector3 inputPos)
+    {
+        Vector2Int vec2 = new Vector2Int((int)(inputPos.x  / chessScale.x+ offestPos.x), (int)(inputPos.y  / chessScale.y+ offestPos.y));
+        vec2.x = Mathf.Clamp(vec2.x, 0, 14);
+        vec2.y = Mathf.Clamp(vec2.y, 0, 14);
+        Debug.Log(vec2);
+        return vec2;
+    }
+}
