3873e0cb13be829c55bdd0a4f8e3c535d68774b4
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-07-06 11:34:45 +0800

    胜利的检测

--- Assets/Scripts/ChessBoardManager.cs
+++ Assets/Scripts/ChessBoardManager.cs
@@ -8,7 +8,16 @@ using UnityEngine;
 /// </summary>
 public class ChessBoardManager : AbsMono
 {
-    private const int chessBoardMaxX = 15, chessBoardMaxY = 15;
+    public enum MoveDir
+    {
+        TransverseLine,
+        VerticalLine,
+        LeftSlantLine,
+        RightSlantLine,
+    }
+
+
+    private const int chessMaxBoard = 15;
     private static readonly Vector2 startPos = new Vector2(-6.58f, -6.45f), chessScale = new Vector2(0.95f, 0.95f)
         , halfChessScale, offestPos;
 
@@ -22,7 +31,7 @@ public class ChessBoardManager : AbsMono
 
     public override void OnAwake()
     {
-        gridArray = new ChessType[chessBoardMaxX, chessBoardMaxY];
+        gridArray = new ChessType[chessMaxBoard, chessMaxBoard];
     }
 
     public bool InputAxisToPoint(Vector3 inputPos, out Vector2Int outPos)
@@ -44,7 +53,7 @@ public class ChessBoardManager : AbsMono
 
     public bool CheckBorder(Vector2Int inputPoint)
     {
-        if ((inputPoint.x >= 0 && inputPoint.x < chessBoardMaxX) || (inputPoint.y >= 0 && inputPoint.y < chessBoardMaxY))
+        if ((inputPoint.x >= 0 && inputPoint.x < chessMaxBoard) && (inputPoint.y >= 0 && inputPoint.y < chessMaxBoard))
         {
             return true;
         }
@@ -59,11 +68,59 @@ public class ChessBoardManager : AbsMono
     public bool PlayChess(Vector2Int inputPos, ChessType _chess)
     {
         gridArray[inputPos.x, inputPos.y] = _chess;
-        return CheckWineer(inputPos);
+        return CheckWineer(inputPos, _chess);
     }
 
-    public bool CheckWineer(Vector2Int inputPos)
+    public bool CheckWineer(Vector2Int inputPos, ChessType _chess)
     {
-        return false;
+        return CheckOneLine(inputPos, _chess, MoveDir.TransverseLine) || CheckOneLine(inputPos, _chess, MoveDir.VerticalLine)
+            || CheckOneLine(inputPos, _chess, MoveDir.LeftSlantLine) || CheckOneLine(inputPos, _chess, MoveDir.RightSlantLine);
+    }
+
+    /// <summary>
+    /// 检测线
+    /// </summary>
+    /// <param name="inputPos"></param>
+    /// <param name="_chess"></param>
+    /// <param name="_moveDir"></param>
+    /// <returns></returns>
+    public bool CheckOneLine(Vector2Int inputPos, ChessType _chess, MoveDir _moveDir)
+    {
+        int chessNumber = 1;
+        int xDir = 0, yDir = 0;
+        switch (_moveDir)
+        {
+            case MoveDir.TransverseLine:
+                xDir = 1; yDir = 0;
+                break;
+            case MoveDir.VerticalLine:
+                xDir = 0; yDir = 1;
+                break;
+            case MoveDir.LeftSlantLine:
+                xDir = 1; yDir = -1;
+                break;
+            case MoveDir.RightSlantLine:
+                xDir = 1; yDir = 1;
+                break;
+            default:
+                break;
+        }
+
+        for (int dir = -1; dir <= 1; dir += 2)
+        {
+            for (int i = 1; i < chessMaxBoard; i++)
+            {
+                Vector2Int newPoint = new Vector2Int(inputPos.x + dir * xDir * i, inputPos.y + dir * yDir * i);
+                if (CheckBorder(newPoint) && gridArray[newPoint.x, newPoint.y] == _chess)
+                {
+                    chessNumber++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        return chessNumber >= 5;
     }
 }
